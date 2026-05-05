e94ddefd30b8cf6407872483da0072b58874773a
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-05 20:08:51 +0300

    Solution

---
+++ app/helper_functions.py
@@ -0,0 +1,11 @@
+import asyncio
+from typing import Any, Awaitable
+
+
+async def run_sequence(*functions: Awaitable[Any]) -> None:
+    for function in functions:
+        await function
+
+
+async def run_parallel(*functions: Awaitable[Any]) -> None:
+    await asyncio.gather(*functions)
