2a5de625524c142273a1d63fe7feef0a4a023bed
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-05 20:39:02 +0300

    Solution3

--- app/iot/message.py
+++ app/iot/message.py
@@ -11,6 +11,7 @@ class MessageType(Enum):
     CLOSE = auto()
     FLUSH = auto()
     CLEAN = auto()
+    STOP = auto()
 
 
 @dataclass
--- app/main.py
+++ app/main.py
@@ -33,13 +33,20 @@ async def main() -> None:
                     "Rick Astley - Never Gonna Give You Up"
                 )
             )
+        ),
+        run_sequence(
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+            service.send_msg(Message(toilet_id, MessageType.CLEAN)),
         )
     )
 
     # sleep
     await run_parallel(
         service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
-        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+        run_sequence(
+            service.send_msg(Message(speaker_id, MessageType.STOP)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+        ),
         run_sequence(
             service.send_msg(Message(toilet_id, MessageType.FLUSH)),
             service.send_msg(Message(toilet_id, MessageType.CLEAN)),
